0edad8791415631acc1628585865592cf01f8498
Scott M Stark <starksm64@gmail.com>
2012-02-07 17:43:16 -0800

    Reorganize into separate modules

---
+++ mysql/build.sh
@@ -0,0 +1,3 @@
+version=5.1.18
+# Create the mysql-${version}.tgz content for use by the jboss-as7-modules RPM
+tar -czf mysql-modules-${version}.tgz modules
---
+++ switchyard/build.sh
@@ -0,0 +1,3 @@
+version=0.3.0
+# Create the switchyard-${version}.tgz content for use by the jboss-as7-modules RPM
+tar -czf switchyard-modules-${version}.tgz modules
