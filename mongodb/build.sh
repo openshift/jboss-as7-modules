2e2601183ca31484e99f2479e665a6c762361586
Scott Stark <sstark@redhat.com>
2012-02-16 17:49:12 -0800

    Update switchyard version in build.sh Add mongodb java driver module

---
+++ mongodb/build.sh
@@ -0,0 +1,3 @@
+version=2.7.3
+# Create the mongodb-${version}.tgz content for use by the jboss-as7-modules RPM
+tar -czf mongodb-java-driver-modules-${version}.tgz modules
--- switchyard/build.sh
+++ switchyard/build.sh
@@ -1,3 +1,3 @@
-version=0.3.0
+version=0.4.0
 # Create the switchyard-${version}.tgz content for use by the jboss-as7-modules RPM
 tar -czf switchyard-modules-${version}.tgz modules
